f1e79b0f0c484123afb7ed15b409ce296cdb23b8
R-CO <daniel1820kobe@gmail.com>
2016-01-20 18:39:06 +0800

    replace some hard-code strings into constant string variables

--- chess_battle/src/chess.cpp
+++ chess_battle/src/chess.cpp
@@ -8,43 +8,49 @@
 #include <map>
 #include <vector>
 
-const std::vector<wxString> kBlackChessNameVector = {wxT("卒"), wxT("包"), wxT("馬"), wxT("車"), wxT("象"), wxT("士"), wxT("將")};
-const std::vector<wxString> kRedChessNameVector = {wxT("兵"), wxT("炮"), wxT("傌"), wxT("俥"), wxT("相"), wxT("仕"), wxT("帥")};
+const std::vector<wxString> kBlackChessNameVector = 
+{
+  wxT("卒") /* 0 */, wxT("包") /* 1 */, wxT("馬") /* 2 */, wxT("車") /* 3 */,	wxT("象") /* 4 */, wxT("士") /* 5 */, wxT("將") /* 6 */
+};
+const std::vector<wxString> kRedChessNameVector = 
+{
+  wxT("兵") /* 0 */, wxT("炮") /* 1 */, wxT("傌") /* 2 */, wxT("俥") /* 3 */, wxT("相") /* 4 */, wxT("仕") /* 5 */, wxT("帥") /* 6 */
+};
 
 std::map<wxString, ChessColor> Chess::kChessNameColorMap = 
 { 
-  std::pair<wxString, ChessColor>(wxT("卒"), kBlackChess),
-  std::pair<wxString, ChessColor>(wxT("包"), kBlackChess),
-  std::pair<wxString, ChessColor>(wxT("馬"), kBlackChess),
-  std::pair<wxString, ChessColor>(wxT("車"), kBlackChess), 
-  std::pair<wxString, ChessColor>(wxT("象"), kBlackChess), 
-  std::pair<wxString, ChessColor>(wxT("士"), kBlackChess), 
-  std::pair<wxString, ChessColor>(wxT("將"), kBlackChess),
-  std::pair<wxString, ChessColor>(wxT("兵"), kRedChess),
-  std::pair<wxString, ChessColor>(wxT("炮"), kRedChess),
-  std::pair<wxString, ChessColor>(wxT("傌"), kRedChess),
-  std::pair<wxString, ChessColor>(wxT("俥"), kRedChess),
-  std::pair<wxString, ChessColor>(wxT("相"), kRedChess),
-  std::pair<wxString, ChessColor>(wxT("仕"), kRedChess),
-  std::pair<wxString, ChessColor>(wxT("帥"), kRedChess)
+  std::pair<wxString, ChessColor>(kBlackChessNameVector[0], kBlackChess), // wxT("卒")
+  std::pair<wxString, ChessColor>(kBlackChessNameVector[1], kBlackChess), // wxT("包")
+  std::pair<wxString, ChessColor>(kBlackChessNameVector[2], kBlackChess), // wxT("馬")
+  std::pair<wxString, ChessColor>(kBlackChessNameVector[3], kBlackChess), // wxT("車")
+  std::pair<wxString, ChessColor>(kBlackChessNameVector[4], kBlackChess), // wxT("象")
+  std::pair<wxString, ChessColor>(kBlackChessNameVector[5], kBlackChess), // wxT("士")
+  std::pair<wxString, ChessColor>(kBlackChessNameVector[6], kBlackChess), // wxT("將")
+  std::pair<wxString, ChessColor>(kRedChessNameVector[0], kRedChess),         // wxT("兵")
+  std::pair<wxString, ChessColor>(kRedChessNameVector[1], kRedChess),         // wxT("炮")
+  std::pair<wxString, ChessColor>(kRedChessNameVector[2], kRedChess),         // wxT("傌")
+  std::pair<wxString, ChessColor>(kRedChessNameVector[3], kRedChess),         // wxT("俥")
+  std::pair<wxString, ChessColor>(kRedChessNameVector[4], kRedChess),         // wxT("相")
+  std::pair<wxString, ChessColor>(kRedChessNameVector[5], kRedChess),         // wxT("仕")
+  std::pair<wxString, ChessColor>(kRedChessNameVector[6], kRedChess)          // wxT("帥")
 };
 
 std::map<wxString, ChessType> Chess::kChessNameTypeMap = 
 {
-  std::pair<wxString, ChessType>(wxT("卒"), 0),
-  std::pair<wxString, ChessType>(wxT("包"), 1),
-  std::pair<wxString, ChessType>(wxT("馬"), 2),
-  std::pair<wxString, ChessType>(wxT("車"), 3),
-  std::pair<wxString, ChessType>(wxT("象"), 4),
-  std::pair<wxString, ChessType>(wxT("士"), 5),
-  std::pair<wxString, ChessType>(wxT("將"), 6),
-  std::pair<wxString, ChessType>(wxT("兵"), 0),
-  std::pair<wxString, ChessType>(wxT("炮"), 1),
-  std::pair<wxString, ChessType>(wxT("傌"), 2),
-  std::pair<wxString, ChessType>(wxT("俥"), 3),
-  std::pair<wxString, ChessType>(wxT("相"), 4),
-  std::pair<wxString, ChessType>(wxT("仕"), 5),
-  std::pair<wxString, ChessType>(wxT("帥"), 6)
+  std::pair<wxString, ChessType>(kBlackChessNameVector[0], 0), // wxT("卒")
+  std::pair<wxString, ChessType>(kBlackChessNameVector[1], 1), // wxT("包")
+  std::pair<wxString, ChessType>(kBlackChessNameVector[2], 2), // wxT("馬")
+  std::pair<wxString, ChessType>(kBlackChessNameVector[3], 3), // wxT("車")
+  std::pair<wxString, ChessType>(kBlackChessNameVector[4], 4), // wxT("象")
+  std::pair<wxString, ChessType>(kBlackChessNameVector[5], 5), // wxT("士")
+  std::pair<wxString, ChessType>(kBlackChessNameVector[6], 6), // wxT("將")
+  std::pair<wxString, ChessType>(kRedChessNameVector[0], 0),     // wxT("兵")
+  std::pair<wxString, ChessType>(kRedChessNameVector[1], 1),     // wxT("炮")
+  std::pair<wxString, ChessType>(kRedChessNameVector[2], 2),     // wxT("傌")
+  std::pair<wxString, ChessType>(kRedChessNameVector[3], 3),     // wxT("俥")
+  std::pair<wxString, ChessType>(kRedChessNameVector[4], 4),     // wxT("相")
+  std::pair<wxString, ChessType>(kRedChessNameVector[5], 5),     // wxT("仕")
+  std::pair<wxString, ChessType>(kRedChessNameVector[6], 6)      // wxT("帥")
 };
 
 void Chess::ResetChess(void)
